a5b84d58dc918a7810e3d2cafc38243f1866228e
dionyguape <dionyguape@gmail.com>
2023-10-15 19:21:08 -0300

    Conectando ao mongoose

---
+++ conectando-ao-mongoose/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Conectando ao banco de dados
+main().catch(err => console.log('Tá fraquejado ' + err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  console.log('Conectado ao banco de dados');
+}
+
